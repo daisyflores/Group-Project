d07d3e4fa3399005bd1146733b3df8f68f9b4f53
daisyflores <35595681+daisyflores@users.noreply.github.com>
2018-01-19 20:41:05 +0100

    Add files via upload

--- ExamA.R
+++ ExamA.R
@@ -1,21 +1,21 @@
 working_directory <-'/Users/Daisy/Desktop/MultivariateStats/multivar'
 
-#Read Data####
+#1.Read Data####
 env <-read.delim(file = paste0(working_directory, '//data2_env.txt'), 
                  h=T, sep ="\t", row.names = 1)
 species <-(read.delim(file = paste0(working_directory, '//data2_species.txt'), 
                       h=T, sep ="\t"))
 
-#Calculate abundance/transect ####
+#2.Calculate density/site ####
 require(reshape) # (load necessary package)
 species.wide<-cast(species,siteID+transect~species,sum)
 species.melted <- melt(species.wide)
 row.names(species.melted) <- 1:length(species.melted[,1])
-names(species.melted) <- c(names(species.melted)[1:2],'density',
-                           names(species.melted)[4])
-species.melted <- subset(species.melted,select=c(1:3))
-species.density<- aggregate(density~ siteID,
+names(species.melted)[3] <- c("abundance")
+#disregards zeros
+species.density<- aggregate(abundance~ siteID,
                             FUN=sum,data=species.melted)
+species.density$density <- species.density$abundance/(50*6) #50m2 * 6 transects
 
 ##Prepare to combine into one table ####
 latitude<- aggregate(Latitude~ siteID,
@@ -27,29 +27,32 @@ all.equal(as.character(latitude$siteID),
 
 #Combine Data
 distribution <- data.frame(species.density,latitude$Latitude)
-distribution.ordered <- distribution[order(distribution$latitude.Latitude),]
-row.names(distribution.ordered) <- 1:length(distribution.ordered[,1])
+row.names(distribution) <- 1:length(distribution[,1])
+names(distribution)[ncol(distribution)] <- "latitude"
+distribution.ordered <- distribution[order(distribution$latitude),]
 
-write.table(distribution, file = "Distribution.Env", row.names = T)
+write.table(distribution, file = "ExamDistribution.Env", row.names = T)
 
 #Numerical Testing####
 library(nortest)  # ...from the package 'nortest'...
 require(nortest)
-ad.test(distribution$density)  # Anderson-Darling test and 
-cvm.test(distribution$density)
+ad.test(distribution$density)  # Anderson-Darling test = 0.04
+cvm.test(distribution$density) #Cramer-von Mises normality test = 0.03
+#Not normal distribution
 
 ###Latitudes North and South#### 
 south <- data.frame(distribution.ordered[1:9,])
 north <- data.frame(distribution.ordered[10:25,])
 #Correlation South
-cor.test(x=as.numeric(south$latitude.Latitude), y=south$density, 
+cor.test(x=as.numeric(south$latitude), y=south$density, 
          method = "spearman" ) 
-plot(x=south$latitude.Latitude, y=south$density, 
+plot(x=south$latitude, y=south$density, 
          method = "spearman" ) ##rho= 0.5439, p-value= 0.1301
 #Correlation North
-cor.test(x=north$latitude.Latitude, y=north$density, 
+cor.test(x=north$latitude, y=north$density, 
     method = "spearman" ) # rho=0.9794118, p-value= <2.2e-16
-plot(x=north$latitude.Latitude, y=north$density)
+plot(x=north$latitude, y=north$density)
 
 #Plot both
-plot(x=distribution.ordered$latitude.Latitude,y=distribution.ordered$density)
+plot(x=distribution.ordered$latitude,y=distribution.ordered$density)
+
